deb1488c1d075b048e32f4c6e24f9b9b36916cb5
aleksl0l <aleksl0l@yandex.ru>
2019-02-19 23:14:07 +0300

    Merge branch 'master' of /home/alex/go/src/quizChallenge with conflicts.

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"flag"
+	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	questionHttpDeliver "quizChallenge/question/delivery/http"
 	questionRepo "quizChallenge/question/repository"
 	questionUcase "quizChallenge/question/usecase"
@@ -17,10 +20,36 @@ import (
 var db *sql.DB
 
 func init() {
-	db, _ = sql.Open("postgres", "dbname=quiz user=quiz password=quiz host=localhost sslmode=disable")
+	var configFile string
+	flag.StringVar(&configFile, "config", "config.json", "-config <file>")
+	viper.SetConfigFile(configFile)
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	dbHost := viper.GetString(`database.host`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+	dbName := viper.GetString(`database.name`)
+	sourceName := fmt.Sprintf("dbname=%s user=%s password=%s host=%s sslmode=disable",
+		dbName,
+		dbUser,
+		dbPass,
+		dbHost,
+	)
+	db, err := sql.Open("postgres", sourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 	defer db.Close()
 
 	e := echo.New()
@@ -32,11 +61,12 @@ func main() {
 	userRepository := userRepo.NewPsqlUserRepository(db)
 	questionRepository := questionRepo.NewPsqlQuestionRepository(db)
 
-	timeoutContext := time.Duration(60) * time.Second
+	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
 	uu := userUcase.NewUserUsecase(userRepository, timeoutContext)
 	qu := questionUcase.NewQuestionUsecase(questionRepository, timeoutContext)
 	userHttpDeliver.NewUserHttpHandler(e, uu)
 	questionHttpDeliver.NewQuestionHttpHandler(e, qu)
 
-	e.Logger.Fatal(e.Start("0.0.0.0:8080"))
+	e.Logger.Fatal(e.Start(viper.GetString("server.address")))
 }
+
