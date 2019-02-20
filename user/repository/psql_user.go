8c11842ecb6b3548b59bc6f0566cde825e6be2f5
aleksl0l <aleksl0l@yandex.ru>
2019-02-21 00:23:11 +0300

    Add scan for new fields

--- user/repository/psql_user.go
+++ user/repository/psql_user.go
@@ -17,16 +17,26 @@ func NewPsqlUserRepository(DB *sql.DB) user.Repository {
 }
 
 func (r *psqlUserRepository) GetByUsername(ctx context.Context, username string) (*models.User, error) {
-	row := r.DB.QueryRow(`SELECT id, username, password, email, real_name, city, age 
+	row := r.DB.QueryRow(`SELECT id, username, password, email, real_name, city, age, game_points, is_disabled_ad
 								FROM user_models WHERE username = $1`, username)
 	u := new(models.User)
-	err := row.Scan(&u.ID, &u.Username, &u.Password, &u.Email, &u.RealName, &u.City, &u.Age)
+	err := row.Scan(
+		&u.ID,
+		&u.Username,
+		&u.Password,
+		&u.Email,
+		&u.RealName,
+		&u.City,
+		&u.Age,
+		&u.GamePoints,
+		&u.IsDisableAd,
+	)
 	return u, err
 }
 
 func (r *psqlUserRepository) Store(ctx context.Context, user *models.User) error {
-	_, err := r.DB.Exec(`INSERT INTO user_models(username, email, password, real_name, city, age)
-								VALUES($1, $2, $3, $4, $5, $6)`,
+	_, err := r.DB.Exec(
+		`INSERT INTO user_models(username, email, password, real_name, city, age) VALUES($1, $2, $3, $4, $5, $6)`,
 		user.Username,
 		user.Email,
 		user.Password,
