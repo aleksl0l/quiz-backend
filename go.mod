dc80c0790723a090608675ed37586d2b89306e1d
aleksl0l <aleksl0l@yandex.ru>
2019-02-24 22:40:37 +0300

    Add sentry

--- go.mod
+++ go.mod
@@ -1,12 +1,18 @@
 module quizChallenge
 
 require (
+	github.com/01walid/echosentry v0.0.0-20180505173336-360e3594b585
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/certifi/gocertifi v0.0.0-20190105021004-abcd57078448 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/getsentry/raven-go v0.2.0
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/labstack/echo v3.3.5+incompatible
 	github.com/labstack/gommon v0.2.8 // indirect
 	github.com/lib/pq v1.0.0
 	github.com/mattn/go-colorable v0.0.9 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/spf13/viper v1.3.1
 	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/01walid/echosentry"
+	"github.com/globalsign/mgo"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
 	"os"
+	gameHttpDeliver "quizChallenge/game/delivery/http"
+	gameRepo "quizChallenge/game/repository"
+	gameUcase "quizChallenge/game/usecase"
+	"quizChallenge/question"
 	questionHttpDeliver "quizChallenge/question/delivery/http"
 	questionRepo "quizChallenge/question/repository"
 	questionUcase "quizChallenge/question/usecase"
+	"quizChallenge/user"
 	userHttpDeliver "quizChallenge/user/delivery/http"
 	userRepo "quizChallenge/user/repository"
 	userUcase "quizChallenge/user/usecase"
@@ -21,52 +28,73 @@ import (
 
 func init() {
 	var configFile string
-	flag.StringVar(&configFile, "config", "config.json", "-config <file>")
+	flag.StringVar(&configFile, "config", "config.json", "-config=<file>")
 	flag.Parse()
 	viper.SetConfigFile(configFile)
 	err := viper.ReadInConfig()
-
+	if err != nil {
+		panic(err)
+	}
 	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	dbHost := viper.GetString(`database.host`)
-	dbUser := viper.GetString(`database.user`)
-	dbPass := viper.GetString(`database.pass`)
-	dbName := viper.GetString(`database.name`)
-	sourceName := fmt.Sprintf("dbname=%s user=%s password=%s host=%s sslmode=disable",
-		dbName,
-		dbUser,
-		dbPass,
-		dbHost,
-	)
-	db, err := sql.Open("postgres", sourceName)
-	if err != nil {
-		log.Fatal(err)
+	var userRepository user.Repository
+	var questionRepository question.Repository
+	if viper.GetBool(`database.use`) {
+		dbHost := viper.GetString(`database.host`)
+		dbUser := viper.GetString(`database.user`)
+		dbPass := viper.GetString(`database.pass`)
+		dbName := viper.GetString(`database.name`)
+		sourceName := fmt.Sprintf("dbname=%s user=%s password=%s host=%s sslmode=disable",
+			dbName,
+			dbUser,
+			dbPass,
+			dbHost,
+		)
+		db, err := sql.Open("postgres", sourceName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = db.Ping()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		defer db.Close()
+		userRepository = userRepo.NewPsqlUserRepository(db)
+		questionRepository = questionRepo.NewPsqlQuestionRepository(db)
 	}
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+	var dbMongo *mgo.Database
+	if viper.GetBool(`mongoDb.use`) {
+		sessionMongo, err := mgo.Dial(viper.GetString("mongoDb.host"))
+		if err != nil {
+			os.Exit(1)
+		}
+		dbMongo = sessionMongo.DB(viper.GetString("mongoDb.name"))
+		userRepository = userRepo.NewMongoUserRepository(dbMongo)
+		questionRepository = questionRepo.NewMongoQuestionRepository(dbMongo)
+		defer sessionMongo.Close()
 	}
-	defer db.Close()
+	gameRepository := gameRepo.NewMongoGameRepository(dbMongo)
 
 	e := echo.New()
+	echosentry.SetDSN(viper.GetString(`sentry.dsn`))
+	e.Use(echosentry.Middleware())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${path} ${latency_human}` + "\n",
 	}))
-	e.Use(middleware.Recover())
-
-	userRepository := userRepo.NewPsqlUserRepository(db)
-	questionRepository := questionRepo.NewPsqlQuestionRepository(db)
 
 	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
 	uu := userUcase.NewUserUsecase(userRepository, timeoutContext)
 	qu := questionUcase.NewQuestionUsecase(questionRepository, timeoutContext)
+	gu := gameUcase.NewGameUsecase(gameRepository, userRepository, timeoutContext)
+
 	userHttpDeliver.NewUserHttpHandler(e, uu)
 	questionHttpDeliver.NewQuestionHttpHandler(e, qu)
+	gameHttpDeliver.NewGameHttpHandelr(e, gu)
 
 	e.Logger.Fatal(e.Start(viper.GetString("server.address")))
 }
