b7aab1ed58bbffc121ce83b26811a5a5df88c079
aleksl0l <aleksl0l@yandex.ru>
2019-03-01 00:55:57 +0300

    Add validation tags for question model

--- models/question.go
+++ models/question.go
@@ -6,7 +6,7 @@ type Question struct {
 	ID       bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
 	Text     string        `json:"text" bson:"text"`
 	Image    string        `json:"image" bson:"image"`
-	Type     string        `json:"type" bson:"type"`
-	Category string        `json:"category" bson:"category"`
-	Answers  []Answer      `json:"answers" bson:"answers"`
+	Type     string        `json:"type" bson:"type" validate:"required"`
+	Category string        `json:"category" bson:"category" validate:"required"`
+	Answers  []Answer      `json:"answers" bson:"answers" validate:"required,min=4"`
 }
