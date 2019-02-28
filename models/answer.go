30f06da429f1b106298cb4dc3f4df80c581aae57
aleksl0l <aleksl0l@yandex.ru>
2019-03-01 00:55:43 +0300

    Add validation tags for answer model

--- models/answer.go
+++ models/answer.go
@@ -4,6 +4,6 @@ import "github.com/globalsign/mgo/bson"
 
 type Answer struct {
 	ID        bson.ObjectId `json:"id,omitempty" bson:"-"`
-	Text      string        `json:"text" bson:"text"`
-	IsCorrect bool          `json:"is_correct" bson:"isCorrect"`
+	Text      string        `json:"text" bson:"text" validate:"required"`
+	IsCorrect bool          `json:"is_correct" bson:"isCorrect" validate:"required"`
 }
